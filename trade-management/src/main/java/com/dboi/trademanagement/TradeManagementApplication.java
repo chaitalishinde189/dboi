e7cf428092fdaca4ec1f50ce989cfef5da18f000
cshinde <Chaitali.Shinde@nice.com>
2021-04-17 19:19:08 +0530

    Adding spring boot framework

---
+++ trade-management/src/main/java/com/dboi/trademanagement/TradeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.dboi.trademanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeManagementApplication.class, args);
+    }
+
+}
